b9a0e1e8d26cca29b89af16800db92903fac51cb
Lucas Alves <jlucas_as@hotmail.com>
2024-03-06 17:30:19 -0300

    Ajuste nome do site

--- controller/pages.php
+++ controller/pages.php
@@ -2,14 +2,14 @@
 function index() {
     view('home', [
         'title' => NOME_SITE . ' - Transporte e Logística',
-        'description' => 'Desenvolvemos a VIAS Log com o compromisso de prestar todos os serviços com a máxima qualidade.'
+        'description' => 'Desenvolvemos a '. NOME_SITE .' com o compromisso de prestar todos os serviços com a máxima qualidade.'
     ]);
 }
 
 function nossosFretes() {	
 	view('nossos-fretes', [
         'title' => 'Fretes disponíveis - '. NOME_SITE . ' - Transporte e Logística',
-        'description' => 'Desenvolvemos a VIAS Log com o compromisso de prestar todos os serviços com a máxima qualidade.',
+        'description' => 'Desenvolvemos a '. NOME_SITE .' com o compromisso de prestar todos os serviços com a máxima qualidade.',
 		'shippings' => mysqli()->query("SELECT shippings.* FROM shippings WHERE is_publish = 1 ORDER BY created_at DESC;")
     ]);
 }
