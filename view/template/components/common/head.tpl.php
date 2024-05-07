b1de880872d81bb4bb83f17e3e0d3cba48d77f82
Lucas Alves <jlucas_as@hotmail.com>
2024-05-07 11:17:03 -0300

    feat: google tag manager

--- view/template/components/common/head.tpl.php
+++ view/template/components/common/head.tpl.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-54F8CMNV');</script>
+        <!-- End Google Tag Manager -->
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="theme-color" content="#efc900">
@@ -26,4 +34,5 @@
         <meta property="og:image" content="<?= IMAGES ?>favicon.png" />
     </head>
     <body>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-54F8CMNV" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <div class="content-wrapper">
